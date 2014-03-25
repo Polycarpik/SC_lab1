1d85f1fb936ff816b550f76ed207817d8dfeb02f
Polina <apolo3neorama>
2014-03-25 17:18:13 +0200

    Initial commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,18 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BigrammCounter a = new BigrammCounter();
-        a.getNormalView(a.counter());
+        String[] c = {"_processed.txt","_ws.txt"};
+        String[] b = {"dostoevsky","remark","uliss"};
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < 3; j++){
+                BigrammCounter a = new BigrammCounter(b[j] + c[i]);
+                a.countingProbability();
+                a.write("Letters_" + b[j] + c[i]);
+
+            }
+        }
+
     }
 
 }
