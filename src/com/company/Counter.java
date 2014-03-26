b8b1fdcf4ce95e971a64815a9bd42f330cb7374f
Polina <apolo3neorama>
2014-03-26 23:00:56 +0200

    again refactored code,works!!!

--- src/com/company/Counter.java
+++ src/com/company/Counter.java
@@ -10,7 +10,6 @@ public class Counter {
 
     private String textName;
     private String text;
-    private int total;
 
     private class Element {
 
@@ -62,36 +61,38 @@ public class Counter {
 //    }
 
     private List<Element> hashMapToArrayOfElements(HashMap<String, Integer> holder) {
+        int total = 0;
         List<Element> elements = new ArrayList<Element>();
         Set<String> bigramms = holder.keySet();
         for (String a : bigramms) {
-            this.total += holder.get(a);
+            total += holder.get(a);
             Element e = new Element(a, holder.get(a));
             elements.add(e);
         }
-        total++;
+        elements.add(new Element("total",total + 1));
         return elements;
     }
 
     private void countingProbability(List<Element> elements) {
-        for (int i = 0; i < elements.size(); i++) {
+        int total = elements.get(elements.size()-1).value;
+        for (int i = 0; i < elements.size() - 1; i++) {
 
             Element a = new Element(elements.get(i).key, elements.get(i).value);
-            a.probability = (double) a.value / (total);
+            a.probability = (double) a.value / total;
             elements.set(i, a);
         }
+//        System.out.println("Total is " + total);
+        elements.remove(elements.size()-1);
         Collections.sort(elements, new Comparator<Element>() {
             public int compare(Element o1, Element o2) {
                 return -(o1.probability.compareTo(o2.probability));
             }
         });
-        for (int i = 0; i < elements.size(); i++) {
-            System.out.println(elements.get(i).key + " = " + elements.get(i).value);
-            System.out.println(elements.get(i).key + " = " + elements.get(i).probability);
-
-        }
-        System.out.println("Total is " + total);
-//        System.out.println("entropy = " + entropyCounter(elements));
+//        for (int i = 0; i < elements.size(); i++) {
+//            System.out.println(elements.get(i).key + " = " + elements.get(i).value);
+//            System.out.println(elements.get(i).key + " = " + elements.get(i).probability);
+//
+//        }
     }
 
     private double entropyCounter(List<Element> elements, int gramm) {
@@ -99,10 +100,10 @@ public class Counter {
         for (int i = 0; i < elements.size(); i++) {
             entropy += elements.get(i).probability * (Math.log(elements.get(i).probability) / Math.log(2));
         }
-        return -entropy / gramm;
+        return (-entropy / gramm);
     }
 
-    private void toFile(String addon,String fileName, List<Element> elements, int gramm) {
+    private void toFile(String addon, String fileName, List<Element> elements, int gramm) {
         try {
             PrintWriter out = new PrintWriter(new File(addon + fileName).getAbsoluteFile());
             try {
@@ -134,7 +135,7 @@ public class Counter {
     public void noncrossingBigrammsCounter() {
         List<Element> elements = hashMapToArrayOfElements(counter(2, 2));
         countingProbability(elements);
-        toFile("noncrossingBigramms", textName, elements, 2);
+        toFile("noncrossingBigramms_", textName, elements, 2);
 
     }
 
